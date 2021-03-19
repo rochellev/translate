4d6e9beddff558cfb2be2dc6c7b9e9ab6f7f85e0
rochellev <rochellevroberts@utexas.edu>
2021-03-19 14:40:41 -0700

    add language store, to behave like redux store

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,3 +1,20 @@
-import React from 'react';
+import React from "react";
 
-export default React.createContext('english');
+const Context = React.createContext("english");
+
+class LanguageStore extends React.Component {
+  // your "store"
+  state = { language: "english" };
+  // change selected language
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
+  // only components wrapped in Provider gets access to context object
+  render() {
+    return (
+      <Context.Provider value={{ ...this.state, onLanguageChange }}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
